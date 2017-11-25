27f6a99e2ab67424b0a75b9d35b4519e8acaccbd
= <=>
2017-11-25 23:14:09 +0100

    Changed approach. States are now obs returned by env.step. Constructed TransitionsBuffer. Changed DQN arch to FC feedforward.

--- bipedal_walker/bipedal_walker.py
+++ bipedal_walker/bipedal_walker.py
@@ -1,7 +1,11 @@
 import gym
 from itertools import count
-import torch, torch.nn as nn, torch.nn.functional as F
+from collections import namedtuple
+import torch, torch.nn as nn
+from torch.autograd import Variable
 import matplotlib.pyplot as plt
+import torch.nn.functional as F
+import numpy as np
 
 NUM_EPISODES = 50
 SCREEN_WIDTH = 400
@@ -9,51 +13,72 @@ SCREEN_LENGTH =600
 
 WINDOW_MAX_Y = 300
 WINDOW_MIN_Y = 200
+TRANSITION_BUFFER_SIZE = 100
 
 class DQN(nn.Module):
     def __init__(self):
         super(DQN, self).__init__()
-        self.conv1 = nn.Conv2d(3, 16, kernel_size=5, stride=2)
-        self.bn1 = nn.BatchNorm2d(16)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
-        self.bn2 = nn.BatchNorm2d(32)
-        self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=2)
-        self.bn3 = nn.BatchNorm2d(32)
-        self.head = nn.Linear(448, 4)
+        self.input = nn.Linear(24, 60)
+        self.hidden1 = nn.Linear(60, 20)
+        self.hidden2 = nn.Linear(20, 10)
+        self.output = nn.Linear(10, 4)
 
     def forward(self,x):
-        x = F.relu(self.bn1(self.conv1(x)))
-        x = F.relu(self.bn2(self.conv2(x)))
-        x = F.relu(self.bn3(self.conv3(x)))
-        return self.head(x.view(x.size(0), -1))
+        x = F.tanh(self.input(x))
+        x = F.tanh(self.hidden1(x))
+        x = F.tanh(self.hidden2(x))
+        return F.tanh(self.output(x))
 
-def get_screen():
-    screen = env.render(mode='rgb_array')[WINDOW_MIN_Y:WINDOW_MAX_Y]
-    screen = screen.transpose(2,0,1) #Change to num_channels, width, height for pytorch
-    print(screen.shape)
-    #plt.imshow(screen)
-    #plt.show()
-    return screen
 
-def get_action():
-    return env.action_space.sample()
+class TransitionBuffer:
+    def __init__(self):
+        self.buffer = []
+
+    def store_transition(self,transition):
+        self.buffer.append(transition)
+        if len(self.buffer) > TRANSITION_BUFFER_SIZE:
+            self.buffer.pop(0)
+
+    def get_transitions(self):
+        return self.buffer
+
 
 def update_model():
     pass
 
+
+def get_state(obs):
+    return Variable(torch.from_numpy(obs).float(), volatile=True)
+
+
 if __name__ == '__main__':
     env = gym.make('BipedalWalker-v2').unwrapped
     env.reset()
+    model = DQN()
+    transition_buffer = TransitionBuffer()
+    Transition = namedtuple("Transition", ("state", "action", "reward", "next_state"))
     for episode in range(NUM_EPISODES):
-        previous_screen = get_screen()
-        current_screen = get_screen()
-        state = current_screen - previous_screen
         env.reset()
-        for i in count():
-            current_screen = get_screen()
-            action = get_action()
+        action = env.action_space.sample()
 
-            obs,reward,done,info = env.step(action)
+        previous_state = get_state(np.zeros(24))
+        current_state = get_state(np.zeros(24))
+
+        for i in count():
+            env.render(mode='rgb_array')
+            action = model(current_state).data
+
+            obs, reward, done, info = env.step(action)
+
+            #Updating states
+            previous_state = current_state
+            current_state = get_state(obs)
+
+            #Storing transition
+            transition_buffer.store_transition(Transition(previous_state,action,reward,current_state))
+
+            #Updating model
+            update_model()
 
             if done is True:
                 break
